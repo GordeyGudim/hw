2b1e7236c88b91b70b2797950e51282b9678812a
GordeyGudim <gordey.gudim@mail.ru>
2025-02-25 19:48:54 +0300

    25.02

--- hw.py
+++ hw.py
@@ -1,20 +1,9 @@
-def a(n=0, s = 0, p = 1):
-    for i in range(100, 1000):
-        if i%17 == 0 and i%10 == 3:
-            n+=1
+#совершенное число
+for n in range(1, 1001):    
+    s=0
+    for i in range(1, (n//2)+1):
+        if n%i == 0:
             s+=i
-            p*=i
-    return {'a':s, 'б': n, 'в':p}
-def b(n=0, s = 0, p = 1):
-    for i in range(100, 1000):
-        if ((i%10)+((i//10)%10)+(i//100)%10)%2 == 0:
-            n+=1
-            s+=i
-            p*=i
-    return {'a':s, 'б':n, 'в':p}
-q = input('a/б:')
-if q == 'a':
-    print(f'result:{a()}')
-else:
-    print(f'result:{b()}')
+    if n == s:
+        print(n)
 
